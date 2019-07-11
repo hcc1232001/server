3575bad3666aaa9e1daf59f2bbb0763e6e9ff845
hcc1232001 <qes4d26@hotmail.com>
2019-07-11 16:47:52 +0800

    first commit

--- index.js
+++ index.js
@@ -1,77 +1,70 @@
-// var http = require("http");
-var express = require("express");
-var mysql = require('mysql');
-var app = express();
 // mqtt test
-var mqtt = require('mqtt');
-var clientId = {
+const mqtt = require('mqtt');
 
-  keepalive: 5,  
-  clientId: clientId,  
-  protocolId: 'MQTT',  
-  protocolVersion: 4,  
-  clean: true,  
-  reconnectPeriod: 1000,  
-  connectTimeout: 30 * 1000,  
+// test mqtt using broker from this site
+// http://www.hivemq.com/demos/websocket-client/?
+// const host = 'tcp://10.0.0.2:1884';
+// mqtt server
+// const host = 'mqtt://iot.eclipse.org:1883';
+// mqtt through ws
+const host = 'ws://iot.eclipse.org:80/ws';
+// a unique random string
+const clientId = 'eaca4d1b-34fc-487f-9fd0-ebdcb55322ff';
+
+const options = {
+  keepalive: 10,
+  clientId: clientId,
+  // protocol but not protocolId?
+  // protocolId: 'mqtt',
+  protocol: 'ws',
+  protocolVersion: 4,
+  clean: true,
+  reconnectPeriod: 1000,
+  connectTimeout: 30 * 1000,
   will: {
-    topic: 'Will',  
-    payload: 'Connection Closed unexpectedly :(',  
-    qos: 0,  
+    topic: 'Will',
+    payload: 'Connection Closed unexpectedly :(',
+    qos: 0,
     retain: false
   },
   rejectUnauthorized: false
 };
-var client  = mqtt.connect('mqtt://broker.mqttdashboard.com', {
-  clientId: 'clientId-0yu4gIjmhr'
-});
 
-client.on('connect', function () {
-  client.subscribe('ioioioio', function (err) {
-    if (!err) {
-      client.publish('ioioioio', 'Hello mqtt')
-      // setInterval(function() {
-      //   client.publish('presence', 'Hello mqtt');
-      // }, 10000);
-    } else {
-      console.log(err);
+const mqttClient = mqtt.connect(host, options);
+mqttClient.on('connect', function() {
+  console.log('connect');
+  mqttClient.subscribe(
+    'Popsense/People',
+    { qos: 0 }, 
+    function(err) {
+      if (err) {
+        console.log('subscribe "Popsense/People" failed');
+      }
     }
-  })
+  );
+  mqttClient.subscribe(
+    'Popsense/Product',
+    { qos: 0 }, 
+    function(err) {
+      if (err) {
+        console.log('subscribe "Popsense/Product" failed');
+      }
+    }
+  );
 })
 
-client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(topic.toString() + ' : ' +message.toString());
-  // client.end()
-})
-
-var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'city_gallery'
-})
-connection.connect();
-
-app.get('/', function (req, res) {
-  // res.send('Hello, World!');
-
-// connection.query('SELECT username from ex402_user', function (err, rows, fields) {
-//   if (err) throw err
-  
-//   console.log('number of users found: ', rows.length);
-// })
-
-  connection.query('SELECT * from ex402_user', function (err, rows, fields) {
-    if (err) throw err
-    
-    res.send('number of users found: ' + JSON.stringify(rows));
-  })
-
-  // connection.end();
-
+mqttClient.on('close', function() {
+  console.log(clientId + 'disconnected');
 });
 
-app.listen(3002, function () {
-  console.log('Example app listening on port 3002!');
-});
+mqttClient.on('message', function(topic, message) {
+  const messageStr = message.toString();
+  const messageJSON = JSON.parse(messageStr);
+  // console.log('typeof' + " :\n" + typeof(messageStr));
+  console.log(topic + " :\n" + JSON.stringify(messageJSON, null, '  '));
+})
 
+mqttClient.on('error', function(err) {
+  console.log(err);
+  mqttClient.end();
+});
